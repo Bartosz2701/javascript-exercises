ec3296f5aa4ba3fe8e9d916531de0c35a8bd9f2f
Bartosz <123456789+odin@users.noreply.github.com>
2026-03-20 20:38:21 +0100

    Excersise Done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let rev = [];
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev.push(str[i]);
+        
+    }
+    return rev.join("")
 };
 
 // Do not edit below this line
