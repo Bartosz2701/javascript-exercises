fff485d4109ee71ce338b4e698f811a1a5d372e1
Bartosz <123456789+odin@users.noreply.github.com>
2026-03-21 21:03:29 +0100

    Extra if for other tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,6 +2,16 @@ const sumAll = function(a, b) { // bierzemy dwie liczby dodatnie
 if (a < 0 || b < 0) {
     return "ERROR";
 }
+
+if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+}
+
+if (a > b) {
+    const c = a;
+    a = b;
+    b = c;
+}
     const sum = (b - a + 1) * (a + b) / 2;
 
 // kazda liczba pomiedzy dwoma liczbami jest dodawana do siebie
