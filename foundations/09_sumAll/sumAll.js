668fdcce6870064627b289394055cace667ed3b1
Bartosz <123456789+odin@users.noreply.github.com>
2026-03-21 20:31:41 +0100

    Excersise Done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) { // bierzemy dwie liczby dodatnie
+if (a < 0 || b < 0) {
+    return "ERROR";
+}
+    const sum = (b - a + 1) * (a + b) / 2;
 
+// kazda liczba pomiedzy dwoma liczbami jest dodawana do siebie
+// podawany jest wynik sumowania
+
+return sum;
 };
 
 // Do not edit below this line
